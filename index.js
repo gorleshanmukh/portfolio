532205e5d6274a73a9695ff4566c6debcfa3598e
Shanmukh Gorle <123002254+Shanmukh801311172@users.noreply.github.com>
2023-03-11 04:11:54 -0500

    ss

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-
+const options = {
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./certificate.crt')
+};
 // define a route to render the index.ejs page
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.get('/.well-known/.pki-validation', (req, res) => {
+app.get('/.well-known/pki-validation/DAB3E16E46BE06051EE87426E023B821.txt', (req, res) => {
     const filePath = path.join(__dirname, 'DAB3E16E46BE06051EE87426E023B821.txt');
 
     // Check if the file exists
@@ -24,16 +28,21 @@ app.get('/.well-known/.pki-validation', (req, res) => {
             return;
         }
 
-        // Determine the MIME type of the file
-        const mimeType = 'text/plain';
+        // Read the contents of the file
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.send('Internal server error');
+                return;
+            }
 
-        // Set the appropriate headers
-        res.setHeader('Content-Type', mimeType);
-        res.setHeader('Content-Disposition', 'attachment; filename="DAB3E16E46BE06051EE87426E023B821.txt"');
+            // Set the appropriate headers
+            const mimeType = 'text/plain';
+            res.setHeader('Content-Type', mimeType);
 
-        // Stream the file to the response
-        const fileStream = fs.createReadStream(filePath);
-        fileStream.pipe(res);
+            // Send the contents of the file as the response body
+            res.send(data);
+        });
     });
 });
 app.get('/.well-known/pki-validation/', (req, res) => {
@@ -42,6 +51,8 @@ app.get('/.well-known/pki-validation/', (req, res) => {
 
 // start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
